209294d91da49a3acd8d74b0a2b3e6bb6acb6171
dan.edan <INTEL@INTEL-PC.(none)>
2012-10-05 11:13:04 +0800

    Added index class, lexicon and templates folder

---
+++ lexicon/en/default.inc.php
@@ -0,0 +1,6 @@
+<?php  
+
+	$_lang['xls_user_import'] = 'XLS User Importer';
+	$_lang['xls_user_import.menu_desc'] = 'Import Users using XLS file';
+
+?>
---
+++ templates/default.tpl
@@ -0,0 +1,1 @@
+<h3>Welcome to XLS Importer</h3>
